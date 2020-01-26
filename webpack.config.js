21f19ad6f1467130d1b017a9df58f74aa0cdad53
hiroshi-sss <hiroshi.weblog@gmail.com>
2020-01-26 17:58:12 +0900

    implement babel-loader and react development

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,50 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const outputPath = path.resolve(__dirname, 'dist')
+const outputPath = path.resolve(__dirname, "dist");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: outputPath
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-                        {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg|ico)$/i,
-                loader: 'url-loader',
-                options: {
-                    limit: 2048,
-                    name: './images/[name].[ext]'
-                }
-            }
-        ]
-    },
-    devServer: {
-        contentBase: outputPath
-    }
-}
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: outputPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        loader: "url-loader",
+        options: {
+          limit: 2048,
+          name: "./images/[name].[ext]"
+        }
+      },
+      {
+          test: /\.html$/,
+          loader: 'html-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: outputPath
+  },
+  plugins: [
+      new HtmlWebPackPlugin({
+          template: './src/index.html',
+          filename: './index.html'
+      })
+  ]
+};
